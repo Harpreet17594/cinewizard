1c6fa2c38db2967880ab35ca34b8d0ad95c9c379
roliveira527 <r.deoliveira527@gmail.com>
2023-09-28 20:40:51 +0100

    implemented movie-list component

---
+++ maincode/src/components/MovieList.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import MovieCard from './MovieCard';
+
+const MoviesList = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const API_KEY = 'b2ceb41d1b13c59c04358418f87cecae';
+    const BASE_URL = 'https://api.themoviedb.org/3';
+
+    fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`)
+      .then(response => response.json())
+      .then(data => {
+        setMovies(data.results);
+      })
+      .catch(error => console.error('Error fetching movies:', error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Popular Movies</h1>
+      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+        {movies.map(movie => (
+          <MovieCard
+            key={movie.id}
+            title={movie.title}
+            overview={movie.overview}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MoviesList;
