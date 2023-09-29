297daaa5e4e086064c637c4eb218101440c68901
Mohamed-Ugas <136655335+Mohamed-Ugas@users.noreply.github.com>
2023-09-29 17:47:59 +0100

    added a background-image to the hero-image

--- maincode/src/components/hero-image.jsx
+++ maincode/src/components/hero-image.jsx
@@ -1,12 +1,13 @@
 // src/components/hero-image.jsx
 import React from 'react';
 import './Hero-image.css';
+import heroImage from './Picture1.jpg';
 
 function Hero() {
   return (
-    <div className="hero">
-      <h1>Your Hero Title</h1>
-      <p>Your Hero Subtitle or Introduction</p>
+    <div className="hero" style={{ backgroundImage: `url(${heroImage})` }}>
+      <h1>CineWizard</h1>
+      <p>Don't know what to watch, look no further!!!</p>
     </div>
   );
 }
