31bb3f0aaf285d0bb07054f83fafa27bf42f7631
Mohamed-Ugas <136655335+Mohamed-Ugas@users.noreply.github.com>
2023-10-03 19:33:27 +0100

    added code for provider on MovieList.jsx

--- maincode/src/components/Footer.jsx
+++ maincode/src/components/Footer.jsx
@@ -11,11 +11,11 @@ function Footer(){
                 <div className="col-lg-4">
                 <form>
                     <span className="stylefont">Stay connected</span>
-                    <div class="input-group mb-3 setwidth">
-                    <span class="input-group-text" id="basic-addon1">@</span>
-                    <input type="text" class="form-control" placeholder="Email Address"  aria-label="Username" aria-describedby="basic-addon1"/>
+                    <div className="input-group mb-3 setwidth">
+                    <span className="input-group-text" id="basic-addon1">@</span>
+                    <input type="text" className="form-control" placeholder="Email Address"  aria-label="Username" aria-describedby="basic-addon1"/>
                    &nbsp;
-                    <button type="submit" class="btn btn-outline-success">Signup</button>
+                    <button type="submit" className="btn btn-outline-success">Signup</button>
                     </div>
                     </form>
                 </div>
--- maincode/src/components/MovieList.jsx
+++ maincode/src/components/MovieList.jsx
@@ -8,18 +8,51 @@ const MoviesList = () => {
     const API_KEY = 'b2ceb41d1b13c59c04358418f87cecae';
     const BASE_URL = 'https://api.themoviedb.org/3';
 
-    fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`)
+    // Construct the query URL for popular movies
+    const queryUrl = `${BASE_URL}/movie/popular?api_key=${API_KEY}`;
+
+    fetch(queryUrl)
       .then(response => response.json())
       .then(data => {
         setMovies(data.results);
-        console.log(data.results);
+        // Fetch provider information for each movie
+        fetchProviderInfo(data.results);
       })
-      .catch(error => console.error('Error fetching movies:', error));
-  }, []);
+      .catch(error => console.error('Error fetching popular movies:', error));
+  }, []); // No dependencies, fetch once when component mounts
+
+  // Function to fetch provider information for each movie
+  const fetchProviderInfo = async (movies) => {
+    const providerData = [];
+    const API_KEY = 'b2ceb41d1b13c59c04358418f87cecae';
+    const BASE_URL = 'https://api.themoviedb.org/3';
+
+    for (const movie of movies) {
+      const response = await fetch(`${BASE_URL}/movie/${movie.id}/watch/providers?api_key=${API_KEY}`);
+      const data = await response.json();
+      const providers = data.results.GB?.flatrate; // Replace 'GB' with your region
+
+      if (providers) {
+        providerData.push({ id: movie.id, providers });
+      }
+    }
+
+    // Update the state with provider information
+    setMovies(prevMovies => {
+      return prevMovies.map(movie => {
+        const providerInfo = providerData.find(provider => provider.id === movie.id);
+        return {
+          ...movie,
+          providers: providerInfo ? providerInfo.providers : [],
+        };
+      });
+    });
+  };
 
   return (
     <div>
       <h1>Popular Movies</h1>
+
       <div style={{ display: 'flex', flexWrap: 'wrap' }}>
         {movies.map(movie => (
           <MovieCard
@@ -27,6 +60,7 @@ const MoviesList = () => {
             title={movie.title}
             overview={movie.overview}
             backdrop_path={movie.backdrop_path}
+            providers={movie.providers} // Pass providers as a prop to MovieCard
           />
         ))}
       </div>
